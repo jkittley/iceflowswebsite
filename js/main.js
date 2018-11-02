36a2344265f38aa5cce908d3e20ea6521d437b27
Jacob Kittley-Davies <jacob@kittley.com>
2018-11-02 16:47:00 +0000

    Init Commit

---
+++ js/main.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+  
+  videoLightning({
+    elements: [
+      {
+        ".video-link": {
+          id: "y-x3d3GZNwXrE",
+          autoplay: true,
+          color: "black",
+          bdColor: "#000000"
+        }
+      }
+    ]
+  });
+
+});
