e0420f33f5985102aa9bc9799fd7a0c3d0f708bc
Jacob Kittley-Davies <jacob@kittley.com>
2018-11-02 21:29:40 +0000

    Smooth scroll too

--- js/main.js
+++ js/main.js
@@ -13,4 +13,14 @@ $(document).ready(function(){
     ]
   });
 
+  $(".scroller").click(function(e) {
+    e.preventDefault();
+    var dest = $(e.target).attr('href');
+    if (dest !== null && dest !== undefined) {
+      $('html, body').animate({
+          scrollTop: $(dest).offset().top
+      }, 1000);
+    }
+  });
+
 });
