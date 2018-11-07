7ddaf59c2910bade86d13cb3be9cba7753e94790
Jacob Kittley-Davies <jacob@kittley.com>
2018-11-07 09:56:28 +0000

    Removed old assets

--- js/main.js
+++ js/main.js
@@ -1,18 +1,4 @@
 $(document).ready(function(){
-  
-  videoLightning({
-    elements: [
-      {
-        ".video-link": {
-          id: "y-x3d3GZNwXrE",
-          autoplay: true,
-          color: "black",
-          bdColor: "#000000"
-        }
-      }
-    ]
-  });
-
   $(".scroller").click(function(e) {
     e.preventDefault();
     var dest = $(e.target).attr('href');
@@ -22,12 +8,4 @@ $(document).ready(function(){
       }, 1000);
     }
   });
-
-  $('.stop-youtube').click(function(){
-    console.log("Stop all videos");
-    $('.youtube-video').each(function(){
-      $(this).attr("src", $(this).attr("src"));
-    });
-  });
-
 });
